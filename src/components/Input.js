983698999e9bab2ca6e289b5dc06fd6b7453acf7
lain-alice <freya6829@gmail.com>
2023-04-07 15:40:22 +0900

    json-server 데이터 추가

---
+++ src/components/Input.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const BREAK_POINT_TABLET = 768;
+
+const InputArea = styled.div`  
+
+  .input-box {
+    display:flex;
+    flex-flow:row wrap;
+    justify-content: center;    
+    align-items: center;
+  }
+  .input-box>input{
+    width:400px;
+    height:80px;
+    border-radius:5px;
+    border:none;
+    margin-right:10px;
+    padding:5px;
+  }
+  .input-box>button{
+    width:100px;
+    height:90px;
+    border-radius:5px;
+    border:none;
+    background-color: #24afff;
+    font-size:20px;
+    font-weight:700;
+    color:#fff;
+    text-align:center;
+  }
+  .input-box>button:hover{
+    background-color: #5affe4;
+    cursor: pointer;
+  }
+  
+  @media only screen and (max-width: ${BREAK_POINT_TABLET}px) {
+    .input-box{
+      
+    }
+  }
+  
+`;
+
+const Input = () => {
+  return (
+    <>
+      <InputArea>
+        <div className="input-box">
+            <input type="text" placeholder="할 일을 입력하세요" />
+            <button>등록</button>
+        </div>
+      </InputArea>
+    </>
+  );
+};
+
+export default Input;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import '../App.css';
 
+import Input from './Input'
+
 const BREAK_POINT_TABLET = 768;
 
 const MainArea = styled.div`
@@ -30,19 +32,10 @@ const MainArea = styled.div`
     justify-content: center;    
     align-items: center;
   }
-  .input-box {
-    display:flex;
-    flex-flow:row wrap;
-    justify-content: center;    
-    align-items: center;
-  }
   
   @media only screen and (max-width: ${BREAK_POINT_TABLET}px) {
     .main-box{
       width:90%;
-    }
-    .input-box{      
-      /* flex-basis:auto; */
     }
   }
   
@@ -54,10 +47,7 @@ const Main = () => {
       <MainArea>
         <div className="main-box">
             <h1>To-Do List</h1>
-            <div className="input-box">
-                <input type="text" placeholder="할 일을 입력하세요" />
-                <button>등록</button>
-            </div>
+            <Input />
         </div>
       </MainArea>
     </>

d619e82de0ace8a02e66fd8c24974424fc3dfbcb
lain-alice <freya6829@gmail.com>
2023-04-13 00:28:28 +0900

    eslint, prettier 설치

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,50 +3,47 @@ import styled from 'styled-components';
 
 const BREAK_POINT_TABLET = 768;
 
-const InputArea = styled.div`  
-
+const InputArea = styled.div`
   .input-box {
-    display:flex;
-    flex-flow:row wrap;
-    justify-content: center;    
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
     align-items: center;
   }
-  .input-box>input{
-    width:400px;
-    height:80px;
-    border-radius:5px;
-    border:none;
-    margin-right:10px;
-    padding:5px;
+  .input-box > input {
+    width: 400px;
+    height: 80px;
+    border-radius: 5px;
+    border: none;
+    margin-right: 10px;
+    padding: 5px;
   }
-  .input-box>button{
-    width:100px;
-    height:90px;
-    border-radius:5px;
-    border:none;
+  .input-box > button {
+    width: 100px;
+    height: 90px;
+    border-radius: 5px;
+    border: none;
     background-color: #24afff;
-    font-size:20px;
-    font-weight:700;
-    color:#fff;
-    text-align:center;
+    font-size: 20px;
+    font-weight: 700;
+    color: #fff;
+    text-align: center;
   }
-  .input-box>button:hover{
+  .input-box > button:hover {
     background-color: #5affe4;
     cursor: pointer;
   }
-  
+
   @media only screen and (max-width: ${BREAK_POINT_TABLET}px) {
-    .input-box{
-      
+    .input-box {
     }
-    input{
-      flex:3 0 0;
+    input {
+      flex: 3 0 0;
     }
-    button{
-      flex:1 0 0;
+    button {
+      flex: 1 0 0;
     }
   }
-  
 `;
 
 const Input = () => {
@@ -54,8 +51,8 @@ const Input = () => {
     <>
       <InputArea>
         <div className="input-box">
-            <input type="text" placeholder="할 일을 입력하세요" />
-            <button>등록</button>
+          <input type="text" placeholder="할 일을 입력하세요" />
+          <button>등록</button>
         </div>
       </InputArea>
     </>

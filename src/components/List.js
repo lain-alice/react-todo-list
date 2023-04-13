6c9296dfab5d8a1bc70e39c6df04aa67b3943945
lain-alice <freya6829@gmail.com>
2023-04-14 03:09:14 +0900

    eslint no-unused-vars: warn

--- src/components/List.js
+++ src/components/List.js
@@ -1,43 +1,43 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
 import useFetch from '../util/useFetch';
 import styled from 'styled-components';
 
 const ListArea = styled.div`
-  
-
-
-  .list-box{margin-top:20px;}
-  .list{
-    width:500px;
-    padding:5px;
-    text-align:center;
-    background-color:#fff;
-    border-radius:5px;
-    margin:5px;
+  .list-box {
+    margin-top: 20px;
   }
-  
+  .list {
+    width: 500px;
+    padding: 5px;
+    text-align: center;
+    background-color: #fff;
+    border-radius: 5px;
+    margin: 5px;
+  }
+
   @media only screen and (max-width: 768px) {
-    .list-box{
-      
+    .list-box {
     }
   }
-  
 `;
 
 function List() {
+  const [data] = useFetch('http://localhost:4000/todos');
 
-    const [data, error] = useFetch('http://localhost:4000/todos');
-
-    return (
+  return (
     <ListArea>
-        <div className="list-box">
-            {data !== undefined && data !== null && data.length > 0 &&
-                data.map((item) => (
-                    <div className="list">{item.text}</div>
-            ))}
-        </div>
+      <div className="list-box">
+        {data !== undefined &&
+          data !== null &&
+          data.length > 0 &&
+          data.map((item) => (
+            <div className="list" key={item.id}>
+              {item.text}
+            </div>
+          ))}
+      </div>
     </ListArea>
-    )
+  );
 }
 
 export default List;
